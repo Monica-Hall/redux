49432a868326821be6b149ebc2e51b4082403ff6
Monica-Hall <monicakes@Monicas-MacBook-Air.local>
2020-11-25 15:43:31 -0700

    copied an object using Object.Assign, completed challenge

---
+++ Step 17/CopyObjWithObjAssign.js
@@ -0,0 +1,31 @@
+const defaultState = {
+    user: 'CamperBot',
+    status: 'offline',
+    friends: '732,982',
+    community: 'freeCodeCamp'
+  };
+  
+  const immutableReducer = (state = defaultState, action) => {
+    switch(action.type) {
+      case 'ONLINE':
+
+      // change code below this line
+        // Don't mutate state here or the tests will fail
+        return Object.assign({}, state, {
+          status: "online"
+        })
+    // change code above this line
+
+      default:
+        return state;
+    }
+  };
+  
+  const wakeUp = () => {
+    return {
+      type: 'ONLINE'
+    }
+  };
+  
+  const store = Redux.createStore(immutableReducer);
+  
