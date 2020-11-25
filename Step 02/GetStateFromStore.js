2246c96b9ebf548b5b64f1db5aba66d5ce99f16c
Monica-Hall <monicakes@Monicas-MacBook-Air.local>
2020-11-25 11:30:25 -0700

    got state from store, completed challenge

---
+++ Step 02/GetStateFromStore.js
@@ -0,0 +1,9 @@
+const store = Redux.createStore(
+    (state = 5) => state
+  );
+  
+  // Change code below this line
+
+  //Use store.getState() to retrieve the state from the store, and assign this to a new variable currentState.
+  
+  const currentState = store.getState(); 
